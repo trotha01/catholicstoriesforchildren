4f1edae0d7acd3ddd4922b108df319f0fbff152e
trotha01 <trevor.rothaus@keatext.com>
2022-07-22 18:23:57 -0700

    Updated the About Us video again

--- src/Main.elm
+++ src/Main.elm
@@ -122,7 +122,7 @@ viewBody =
                         , style "height" "100%"
                         , style "top" "0"
                         , style "left" "0"
-                        , src "https://www.youtube.com/embed/GEaq7BbsDSc"
+                        , src "https://www.youtube.com/embed/uxP6KFTER60"
                         , title "About Catholic Stories for Children"
                         , property "frameborder" (Json.Encode.string "0")
                         , property "allow" (Json.Encode.string "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture")
