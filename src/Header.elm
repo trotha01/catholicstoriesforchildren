96ebf4de9d37095bfacdb3795fb11b569d60927a
trotha01 <trevor.rothaus@keatext.com>
2023-01-18 16:35:01 -0800

    Update subpage header style

--- src/Header.elm
+++ src/Header.elm
@@ -40,13 +40,9 @@ viewSubpageHeader currentPage leftMargin =
     header
         [ style "background-color" "#3d5d75"
         , style "background-image" "linear-gradient(130deg, #9DE2EB , #EBD6F1)"
-        , class "relative"
         , style "height" "48px"
-
-        -- overflow hidden is so that the header includes the content, otherwise it collapses
-        , style "overflow" "hidden"
         , style "padding" ("0px " ++ toPx leftMargin)
-        , style "font-size" "19px"
+        , class "grid grid-cols-2 md:grid-cols-[50px_1fr_200px] items-center"
         ]
         [ viewLogo
         , viewHeaderTitle currentPage
@@ -58,16 +54,16 @@ viewHeaderTitle : String -> Html msg
 viewHeaderTitle title =
     a
         [ class "mobileHide"
-        , class "center"
-        , class "inlineBlock"
         , style "text-decoration" "none"
         , class "colorDarkGray"
+        , class "hidden md:inline"
+        , class "justify-self-center"
         , href "/"
         ]
         [ h1
             [ style "font-family" "hvdComicSerifPro"
-            , style "font-size" "34px"
             , style "margin" "0px"
+            , class "text-2xl"
             ]
             [ text "Catholic Stories for Children" ]
         ]
@@ -76,16 +72,19 @@ viewHeaderTitle title =
 backButton : Html msg
 backButton =
     a
-        [ class "vcenter"
-        , style "right" "32px"
-        , style "text-decoration" "none"
+        [ -- class "vcenter"
+          -- , style "right" "32px"
+          style "text-decoration" "none"
         , href "/"
         , class "colorDarkGray"
+        , class "justify-self-end"
+        , class "grid grid-cols-2 items-center justify-items-center"
         ]
         [ img
             [ src "/assets/backarrow.png"
             , style "height" "14px"
-            , style "margin-right" "10px"
+
+            -- , style "margin-right" "10px"
             ]
             []
         , span
@@ -97,9 +96,9 @@ backButton =
 viewLogo : Html.String.Html msg
 viewLogo =
     a
-        [ class "vcenter"
-        , style "margin-left" "30px"
-        , style "text-decoration" "none"
+        [ -- class "vcenter"
+          -- , style "margin-left" "30px"
+          style "text-decoration" "none"
         , class "colorDarkGray"
         , href "/"
         , attribute "aria-label" "home"
