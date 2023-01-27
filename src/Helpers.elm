593d99221ccf60806e32cfc71c56c688eb0601ea
trotha01 <trevor.rothaus@keatext.com>
2023-01-26 20:56:35 -0800

    Add alt text to social media icons

--- src/Helpers.elm
+++ src/Helpers.elm
@@ -63,71 +63,72 @@ toPx x =
 
 instagramLogo : Html msg
 instagramLogo =
-    favicon "https://www.instagram.com/favicon.ico"
+    favicon "instagram" "https://www.instagram.com/favicon.ico"
 
 
 facebookLogo : Html msg
 facebookLogo =
-    favicon "https://www.facebook.com/favicon.ico"
+    favicon "facebook" "https://www.facebook.com/favicon.ico"
 
 
 twitterLogo : Html msg
 twitterLogo =
-    favicon "https://www.twitter.com/favicon.ico"
+    favicon "twitter" "https://www.twitter.com/favicon.ico"
 
 
 linkedInLogo : Html msg
 linkedInLogo =
-    favicon "https://www.linkedin.com/favicon.ico"
+    favicon "linkedin" "https://www.linkedin.com/favicon.ico"
 
 
 vimeoLogo : Html msg
 vimeoLogo =
-    favicon "https://vimeo.com/favicon.ico"
+    favicon "vimeo" "https://vimeo.com/favicon.ico"
 
 
 imdbLogo : Html msg
 imdbLogo =
-    favicon "https://www.imdb.com/favicon.ico"
+    favicon "imdb" "https://www.imdb.com/favicon.ico"
 
 
 youtubeLogo : Html msg
 youtubeLogo =
-    favicon "https://www.youtube.com/favicon.ico"
+    favicon "youtube" "https://www.youtube.com/favicon.ico"
 
 
 pinterestLogo : Html msg
 pinterestLogo =
-    favicon "https://www.pinterest.com/favicon.ico"
+    favicon "pinterest" "https://www.pinterest.com/favicon.ico"
 
 
 spotifyLogo : Html msg
 spotifyLogo =
-    favicon "https://www.spotify.com/favicon.ico"
+    favicon "spotify" "https://www.spotify.com/favicon.ico"
 
 
 soundcloudLogo : Html msg
 soundcloudLogo =
-    favicon "https://soundcloud.com/favicon.ico"
+    favicon "soundcloud" "https://soundcloud.com/favicon.ico"
 
 
 behanceLogo : Html msg
 behanceLogo =
-    favicon "https://www.behance.net/favicon.ico"
+    favicon "behance" "https://www.behance.net/favicon.ico"
 
 
 paypalLogo : Html msg
 paypalLogo =
-    favicon "https://www.paypal.com/favicon.ico"
+    favicon "paypal" "https://www.paypal.com/favicon.ico"
 
 
-favicon : String -> Html msg
-favicon link =
+favicon : String -> String -> Html msg
+favicon alternativeText link =
     img
         [ style "aria-hidden" "true"
         , src link
         , style "width" "16px"
         , style "height" "16px"
+        , alt alternativeText
         ]
         []
 
