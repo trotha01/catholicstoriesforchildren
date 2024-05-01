f3b6f02ecc7726ed281abcc237778dda69b7ade5
trotha01 <trevor.rothaus@keatext.com>
2024-05-01 05:05:03 -0700

    updated opportunities page

--- src/Opportunities/Main.elm
+++ src/Opportunities/Main.elm
@@ -45,18 +45,18 @@ viewBody : Html Never
 viewBody =
     div
         [ class "m-auto"
-        , class "max-w-3xl py-5 px-11"
+        , class "max-w-3xl py-5 sm:px-11"
         ]
-        [ h1 [ class "mb-10" ]
+        [ h1 [ class "mb-10 px-11 sm:px-0" ]
             [ text "Opportunities" ]
 
         -- <iframe src="https://forms.clickup.com/18021848/f/h5zer-2071/DJP4QJE3I8SY4M6JV3" onwheel="" width="100%" height="100%" style="background: transparent; border: 1px solid #ccc;"></iframe>
-        , div [ style "height" "1150px" ]
+        , div [ style "height" "1400px" ]
             [ iframe
                 [ src "https://forms.clickup.com/18021848/f/h5zer-2071/DJP4QJE3I8SY4M6JV3"
                 , style "background" "transparent"
                 , style "width" "100%"
-                , style "height" "1150px"
+                , style "height" "1400px"
                 , style "border" "1px solid #ccc"
                 , attribute "class" "clickup-embed"
                 ]
