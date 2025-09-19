36feb635de0e48093faa26313d311706dd17ecbd
Trevor Rothaus <trotha01@users.noreply.github.com>
2025-09-19 08:32:46 -0700

    attempt to fix downloads. (#12)
    * attempt to fix downloads.
    
    * Attempt to serve pdfs on netlify correctly
    
    * moving rules around in redirect for pdfs
    
    * remove 404.html page
    
    * move printables to public folder
    
    * Move redirects file to public directory
    
    * fix home page links

--- src/Main.elm
+++ src/Main.elm
@@ -61,6 +61,7 @@ type Page
     | Feasts
     | Saints
     | Press
+    | Download
     | NotFound
 
 
@@ -97,104 +98,93 @@ init flags url key =
         ( feastsPageModel, feastsPageCmd ) =
             FeastsPage.init flags url key
 
-        ( isRedirectedUrl, newPath, redirectedUrl ) =
-            -- we use redirectUrl for github. We won't need this when we switch to netlify
-            redirectUrl url
-
-        urlString =
-            Url.toString url
-
         initialPage =
-            if String.contains "animations" urlString then
-                Productions
-
-            else if String.contains "navigation" urlString then
-                Navigation
-
-            else if String.contains "give" urlString then
-                Give
-
-            else if String.contains "contact" urlString then
-                Contact
-
-            else if String.contains "team" urlString then
-                AboutUs
-
-            else if String.contains "resources" urlString then
-                Resources
-
-            else if String.contains "prayers" urlString then
-                Prayers
-
-            else if String.contains "angelus" urlString then
-                Angelus
-
-            else if String.contains "shop" urlString then
-                Shop
-                -- --
-                -- Need to wait until netlify to add feastdayactivities to the SPA. The github 404 redirect doesn't current work with url queries
-                -- else if String.contains "feastdayactivities" urlString then
-                --     ( { model | url = url, page = Feasts }, Cmd.batch [ Nav.pushUrl model.key (Url.toString url), scrollToTopCmd ] )
-
-            else if String.contains "saints" urlString then
-                Saints
-
-            else if String.contains "press" urlString then
-                Press
-
-            else
-                Home
-
-        initModel =
-            { key = key
-            , url = redirectedUrl
-            , signup = Signup.init
-            , page =
-                initialPage
-            , time = Time.millisToPosix 0
-            , timezone = Time.utc
-            , language = English
-            , saintsPageModel = saintsPageModel
-            , feastsPageModel = feastsPageModel
-            , animationsPageModel = animationsPageModel
-            }
-
-        ( redirectedModel, redirectedMsg ) =
-            if isRedirectedUrl then
-                update (LinkClicked (Browser.Internal redirectedUrl)) initModel
-
-            else
-                ( initModel, Cmd.none )
+            parseUrl url
     in
-    ( redirectedModel
-    , Cmd.batch
-        [ Task.perform NewTime Time.now
-        , Task.perform NewZone Time.here
-        , (if initialPage == Productions then Cmd.map ProductionsMsg animationsPageCmd else Cmd.none)
-        , (if initialPage == Saints then Cmd.map SaintsMsg saintsPageCmd else Cmd.none)
-        , (if initialPage == Feasts then Cmd.map FeastsMsg feastsPageCmd else Cmd.none)
-        , redirectedMsg
-        ]
-    )
+        ( { key = key
+          , url = url
+          , signup = Signup.init
+          , page = initialPage
+          , time = Time.millisToPosix 0
+          , timezone = Time.utc
+          , language = English
+          , saintsPageModel = saintsPageModel
+          , feastsPageModel = feastsPageModel
+          , animationsPageModel = animationsPageModel
+          }
+        , Cmd.batch
+            [ Task.perform NewTime Time.now
+            , Task.perform NewZone Time.here
+            , (if initialPage == Productions then Cmd.map ProductionsMsg animationsPageCmd else Cmd.none)
+            , (if initialPage == Saints then Cmd.map SaintsMsg saintsPageCmd else Cmd.none)
+            , (if initialPage == Feasts then Cmd.map FeastsMsg feastsPageCmd else Cmd.none)
+            ]
+        )
+-- Detect downloadable content by file extension
+isDownloadable : String -> Bool
+isDownloadable path =
+    let
+        fileExtensions = [ ".pdf", ".jpg", ".jpeg", ".png", ".webp", ".svg", ".mp4", ".doc", ".docx" ]
+    in
+    List.any (\ext -> String.endsWith ext path) fileExtensions
+
+-- Route parser for SPA and downloadable content
+parseUrl : Url.Url -> Page
+parseUrl url =
+    let
+        path = url.path
+        urlString = Url.toString url
+    in
+    if isDownloadable path then
+        Download
+    else if String.contains "animations" urlString then
+        Productions
+    else if String.contains "navigation" urlString then
+        Navigation
+    else if String.contains "give" urlString then
+        Give
+    else if String.contains "contact" urlString then
+        Contact
+    else if String.contains "team" urlString then
+        AboutUs
+    else if String.contains "resources" urlString then
+        Resources
+    else if String.contains "prayers" urlString then
+        Prayers
+    else if String.contains "angelus" urlString then
+        Angelus
+    else if String.contains "shop" urlString then
+        Shop
+    else if String.contains "saints" urlString then
+        Saints
+    else if String.contains "press" urlString then
+        Press
+    else
+        Home
 
 
-redirectUrl : Url.Url -> ( Bool, String, Url.Url )
-redirectUrl url =
-    case url.query of
-        Just q ->
-            if String.contains "redirect" q then
-                let
-                    newPath =
-                        String.dropLeft 9 q
-                in
-                ( True, newPath, { url | path = newPath, query = Nothing } )
 
-            else
-                ( False, "", url )
+-- redirectUrl : Url.Url -> ( Bool, String, Url.Url )
+-- redirectUrl url =
+--     case url.query of
+--         Just q ->
+--             if String.contains "redirect" q then
+--                 let
+--                     newPath =
+--                         String.dropLeft 9 q
+--                 in
+--                 ( True, newPath, { url | path = newPath, query = Nothing } )
 
-        Nothing ->
-            ( False, "", url )
+--             else
+--                 ( False, "", url )
 
+--         Nothing ->
+--             ( False, "", url )
+
+scrollToTopCmd : Cmd Msg
+scrollToTopCmd =
+    Dom.setViewport 0 0
+    |> Task.perform (\_ -> NoOp)
 
 type Msg
     = LinkClicked Browser.UrlRequest
@@ -216,19 +206,27 @@ update msg model =
             case urlRequest of
                 Browser.Internal url ->
                     updatePage model url
-
                 Browser.External href ->
                     ( model, Nav.load href )
 
         UrlChanged url ->
-            loadPage model url
+            let
+                newPage = parseUrl url
+                urlString = Url.toString url
+            in
+                case newPage of
+                    Download ->
+                        ( model, Nav.load urlString )
+                    _ ->
+                        ( { model | url = url, page = newPage }
+                        , scrollToTopCmd
+                        )
 
         SignupMsg signupMsg ->
             let
-                ( signup, cmd ) =
-                    Signup.update signupMsg model.signup
+                ( updatedSignup, cmd ) = Signup.update signupMsg model.signup
             in
-            ( { model | signup = signup }, cmd |> Cmd.map SignupMsg )
+                ( { model | signup = updatedSignup }, Cmd.map SignupMsg cmd )
 
         NewTime t ->
             ( { model | time = t }, Cmd.none )
@@ -241,80 +239,68 @@ update msg model =
 
         SaintsMsg saintsMsg ->
             let
-                ( updatedSaintsModel, cmd ) =
-                    SaintsPage.update saintsMsg model.saintsPageModel
+                ( updatedSaintsModel, cmd ) = SaintsPage.update saintsMsg model.saintsPageModel
             in
-            ( { model | saintsPageModel = updatedSaintsModel }, Cmd.map SaintsMsg cmd )
+                ( { model | saintsPageModel = updatedSaintsModel }, Cmd.map SaintsMsg cmd )
 
         FeastsMsg feastsMsg ->
             let
-                ( updatedFeastsModel, cmd ) =
-                    FeastsPage.update feastsMsg model.feastsPageModel
+                ( updatedFeastsModel, cmd ) = FeastsPage.update feastsMsg model.feastsPageModel
             in
-            ( { model | feastsPageModel = updatedFeastsModel }, Cmd.map FeastsMsg cmd )
+                ( { model | feastsPageModel = updatedFeastsModel }, Cmd.map FeastsMsg cmd )
 
         ProductionsMsg productionsMsg ->
             let
-                ( updatedProductionsModel, cmd ) =
-                    Animations.View.update productionsMsg model.animationsPageModel
+                ( updatedProductionsModel, cmd ) = Animations.View.update productionsMsg model.animationsPageModel
             in
-            ( { model | animationsPageModel = updatedProductionsModel }, Cmd.map ProductionsMsg cmd )
+                ( { model | animationsPageModel = updatedProductionsModel }, Cmd.map ProductionsMsg cmd )
 
         NoOp ->
             ( model, Cmd.none )
-
-
-scrollToTopCmd : Cmd Msg
-scrollToTopCmd =
-    Dom.setViewport 0 0
-        |> Task.perform (\_ -> NoOp)
-
 
 updatePage : Model -> Url.Url -> ( Model, Cmd Msg )
 updatePage model url =
     let
         urlString =
             Url.toString url
+        isFileLink =
+            let
+                fileExtensions = [ ".pdf", ".jpg", ".jpeg", ".png", ".webp", ".svg", ".mp4", ".doc", ".docx" ]
+            in
+            List.any (\ext -> String.endsWith ext urlString) fileExtensions
     in
-    if String.contains "animations" urlString then
+    if isFileLink then
+        ( model, Cmd.batch [ Nav.pushUrl model.key (Url.toString url) ]) 
+    else if String.contains "animations" urlString then
         ( { model | url = url, page = Productions }, Cmd.batch [ Nav.pushUrl model.key (Url.toString url), scrollToTopCmd ] )
-
     else if String.contains "navigation" urlString then
         ( { model | url = url, page = Navigation }, Cmd.batch [ Nav.pushUrl model.key (Url.toString url), scrollToTopCmd ] )
-
     else if String.contains "give" urlString then
         ( { model | url = url, page = Give }, Cmd.batch [ Nav.pushUrl model.key (Url.toString url), scrollToTopCmd ] )
-
     else if String.contains "contact" urlString then
         ( { model | url = url, page = Contact }, Cmd.batch [ Nav.pushUrl model.key (Url.toString url), scrollToTopCmd ] )
-
     else if String.contains "team" urlString then
         ( { model | url = url, page = AboutUs }, Cmd.batch [ Nav.pushUrl model.key (Url.toString url), scrollToTopCmd ] )
-
     else if String.contains "resources" urlString then
         ( { model | url = url, page = Resources }, Cmd.batch [ Nav.pushUrl model.key (Url.toString url), scrollToTopCmd ] )
-
     else if String.contains "prayers" urlString then
         ( { model | url = url, page = Prayers }, Cmd.batch [ Nav.pushUrl model.key (Url.toString url), scrollToTopCmd ] )
-
     else if String.contains "angelus" urlString then
         ( { model | url = url, page = Angelus }, Cmd.batch [ Nav.pushUrl model.key (Url.toString url), scrollToTopCmd ] )
-
     else if String.contains "shop" urlString then
         ( { model | url = url, page = Shop }, Cmd.batch [ Nav.pushUrl model.key (Url.toString url), scrollToTopCmd ] )
         -- --
         -- Need to wait until netlify. The github 404 redirect doesn't current work with url queries
         -- else if String.contains "feastdayactivities" urlString then
         --     ( { model | url = url, page = Feasts }, Cmd.batch [ Nav.pushUrl model.key (Url.toString url), scrollToTopCmd ] )
-
     else if String.contains "saints" urlString then
         ( { model | url = url, page = Saints }, Cmd.batch [ Nav.pushUrl model.key (Url.toString url), scrollToTopCmd ] )
-
     else if String.contains "press" urlString then
         ( { model | url = url, page = Press }, Cmd.batch [ Nav.pushUrl model.key (Url.toString url), scrollToTopCmd ] )
-
+    else if url.path == "/" then
+        ( { model | url = url, page = Home }, Cmd.batch [ Nav.pushUrl model.key (Url.toString url), scrollToTopCmd ] )
     else
-        ( model, Nav.load (Url.toString url) )
+        ( model, Cmd.none )
 
 
 loadPage : Model -> Url.Url -> ( Model, Cmd Msg )
@@ -323,45 +309,40 @@ loadPage model url =
     let
         urlString =
             Url.toString url 
+        isFileLink =
+            let
+                fileExtensions = [ ".pdf", ".jpg", ".jpeg", ".png", ".webp", ".svg", ".mp4", ".doc", ".docx" ]
+            in
+            List.any (\ext -> String.endsWith ext urlString) fileExtensions
     in
-
-    if String.contains "animations" urlString then
+    if isFileLink then
+        ( model, Cmd.none )
+    else if String.contains "animations" urlString then
         ( { model | url = url, page = Productions }, scrollToTopCmd )
-
     else if String.contains "navigation" urlString then
         ( { model | url = url, page = Navigation }, scrollToTopCmd )
-
     else if String.contains "give" urlString then
         ( { model | url = url, page = Give }, scrollToTopCmd )
-
     else if String.contains "contact" urlString then
         ( { model | url = url, page = Contact }, scrollToTopCmd )
-
     else if String.contains "team" urlString then
         ( { model | url = url, page = AboutUs }, scrollToTopCmd )
-
     else if String.contains "resources" urlString then
         ( { model | url = url, page = Resources }, scrollToTopCmd )
-
     else if String.contains "prayers" urlString then
         ( { model | url = url, page = Prayers }, scrollToTopCmd )
-
     else if String.contains "angelus" urlString then
         ( { model | url = url, page = Angelus }, scrollToTopCmd )
-
     else if String.contains "shop" urlString then
         ( { model | url = url, page = Shop }, scrollToTopCmd )
         -- --
         -- Need to wait until netlify. The github 404 redirect doesn't current work with url queries
         -- else if String.contains "feastdayactivities" urlString then
         --     ( { model | url = url, page = Feasts }, scrollToTopCmd )
-
     else if String.contains "saints" urlString then
         ( { model | url = url, page = Saints }, scrollToTopCmd )
-
     else if String.contains "press" urlString then
         ( { model | url = url, page = Press }, scrollToTopCmd )
-
     else
         ( model, Cmd.none )
 
@@ -387,62 +368,45 @@ view model =
             case model.page of
                 Home ->
                     viewHome model
-
                 Navigation ->
                     { title = "Navigation", body = [ NavigationPage.view ] }
-
                 Give ->
                     { title = "Donate", body = [ GivePage.view ] }
-
                 Contact ->
                     { title = "Contact Us", body = [ ContactPage.view ] }
-
                 AboutUs ->
                     { title = "About Us", body = [ TeamPage.view ] }
-
                 Shop ->
                     { title = "Shop", body = [ ShopPage.view ] }
-
                 Resources ->
                     { title = "Resources", body = [ ResourcesPage.view model.url ] }
-
                 Prayers ->
                     { title = "Prayers", body = [ PrayersPage.view ] }
-
                 Angelus ->
                     { title = "Angelus", body = [ AngelusPage.view ] }
-
                 Saints ->
                     let
-                        saintPageModel =
-                            model.saintsPageModel
-
-                        document =
-                            SaintsPage.view { saintPageModel | url = model.url }
+                        saintPageModel = model.saintsPageModel
+                        document = SaintsPage.view { saintPageModel | url = model.url }
                     in
                     { title = document.title, body = document.body |> List.map (Html.map SaintsMsg) }
-
                 Feasts ->
                     let
-                        document =
-                            FeastsPage.view model.feastsPageModel
+                        document = FeastsPage.view model.feastsPageModel
                     in
                     { title = document.title, body = document.body |> List.map (Html.map FeastsMsg) }
-
                 Productions ->
                     let
-                        document =
-                            Animations.View.view model.url model.animationsPageModel
+                        document = Animations.View.view model.url model.animationsPageModel
                     in
                     { title = document.title, body = document.body |> List.map (Html.map ProductionsMsg) }
-
                 Press ->
                     { title = "Angelus", body = [ ViewPress.view ] }
-
+                Download ->
+                    { title = "Download", body = [ div [ class "p-10 text-center" ] [ text "This is a downloadable file. If it does not open automatically, please check your browser's download bar or try the direct link again." ] ] }
                 NotFound ->
                     let
-                        document =
-                            NotFound.Main.view
+                        document = NotFound.Main.view
                     in
                     { title = "Tony Help, Page Not Found", body = [ Html.map (\_ -> NoOp) document ] }
     in
